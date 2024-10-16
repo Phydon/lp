38774b3cea305654b251653ca1af08aef254dd4a
Phydon <leann.phydon@gmail.com>
2024-10-16 10:25:39 +0200

    search in files for specified words

--- lp.c
+++ lp.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
 
-void listFilesRecursively(const char *directory) {
+// TODO adjust if needed
+#define MAX_LINE_LENGTH 1024
+
+// TODO
+// Function to search for the word "todo" in a file
+void scanfile(const char *filePath, const char *words[], int wordCount) {
+    FILE *file = fopen(filePath, "r");
+    if (file == NULL) {
+        printf("Failed to open file: %s\n", filePath);
+        return;
+    }
+
+    char line[MAX_LINE_LENGTH];
+    int lineNumber = 0;
+    while (fgets(line, sizeof(line), file)) {
+        lineNumber++;
+        for (int i = 0; i < wordCount; i++) {
+            if (strstr(line, words[i]) != NULL) {
+                printf("Leak found: '%s' in: '%s' (Line %d): %s", words[i], filePath, lineNumber, line);
+            }
+        }
+    }
+
+    fclose(file);
+}
+
+void listFilesRecursively(const char *directory, const char *words[], int wordCount) {
     // A structure that stores information about the file or directory
     WIN32_FIND_DATA findFileData;
     HANDLE hFind = INVALID_HANDLE_VALUE;
@@ -30,9 +57,10 @@ void listFilesRecursively(const char *directory) {
             // Check if it's a directory or a file
             if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
                 // It's a directory, recursively list its contents
-                listFilesRecursively(fullPath);
+                listFilesRecursively(fullPath, words, wordCount);
             } else {
-                printf("%s\n", fullPath);
+                // printf("%s\n", fullPath);
+                scanfile(fullPath, words, wordCount);
             }
         }
     } while (FindNextFile(hFind, &findFileData) != 0);
@@ -42,6 +70,11 @@ void listFilesRecursively(const char *directory) {
 }
 
 int main(int argc, char **argv) {
+    // Define the list of words to search for
+    // TODO change search patterns later
+    const char *words[] = {"todo", "fixme", "info"};
+    int wordCount = sizeof(words) / sizeof(words[0]);
+
     // print usage if the directory name isn't specified
     if (argc != 2) {
         printf("Usage: %s <PATH>", argv[0]);
@@ -49,7 +82,7 @@ int main(int argc, char **argv) {
     }
 
     // List all files in the specified directory recursively
-    listFilesRecursively(argv[1]);
+    listFilesRecursively(argv[1], words, wordCount);
 
     return 0;
 }
